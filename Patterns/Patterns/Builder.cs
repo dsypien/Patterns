786f1928a6b0fce1b7594af7d514903af419d5fa
dsypien <davidsypien@gmail.com>
2013-12-06 08:58:18 -0500

    Add Builder Pattern

---
+++ Patterns/Patterns/Builder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    /// <summary>
+    /// Builder Design Pattern
+    /// </summary>
+    public class Builder
+    {
+        struct Point
+        {
+            int X{get;set;}
+            int Y{get;set;}
+        }
+
+        private sealed class Period
+        {
+            // make name immutable
+            private readonly string name;
+
+            // Private Constructor so that only possible way to construct a Period is using the Builder
+            private Period(Builder builder)
+            {
+                this.name = builder.Name;
+            }
+
+            public sealed class Builder
+            {
+                public string Name { get; set; }
+
+                public Period Build()
+                {
+                    return new Period(this);
+                }
+            }
+        }
+    }
+}
--- Patterns/Patterns/State.cs
+++ Patterns/Patterns/State.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Patterns
 {
+    // State Pattern
     public class State
     {
     }
